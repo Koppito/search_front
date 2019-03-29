780ce70061902f0f4614adac9b9d3923975cc667
MatiasKopp <matias.kopp@mercadolibre.com>
2019-03-29 17:00:21 -0300

    Added common.css and added some color to home page

--- src/pages/document/index.js
+++ src/pages/document/index.js
@@ -1,7 +1,6 @@
 import React, {Component} from "react";
 
 class Document extends Component {
-
     render() {
         return (
             <div><h1>Document!</h1></div>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,20 @@
 import React, {Component} from "react";
+import "./styles/main.css";
 
 class Home extends Component {
-
     render() {
         return (
-            <div><h1>Home!</h1></div>
+            <div className="search-center">
+                <div className="search-container">
+                    <div className="search-title">
+                        <h1>yet another <span style={{ color: "#ff7f50", fontWeight: "bold"}}>Search Engine</span></h1>
+                    </div>
+                    <div className="search-bar">
+                        <input className="search-text" type="text"></input>
+                        <button className="search-button">Search</button>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
