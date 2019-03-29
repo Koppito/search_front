ce3ab3077b1d68e0d0c8c1138d9fafde75c41aa6
MatiasKopp <matias.kopp@mercadolibre.com>
2019-03-29 14:14:54 -0300

    Added initial routes to Router and Switch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
-import Home from './pages/home';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/home";
+import Search from "./pages/search";
+import Document from "./pages/document";
+import NotFound from "./pages/not_found";
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/search" component={Search}></Route>
+          <Route path="/document" component={Document}></Route>
+          <Route component={NotFound}></Route>
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ src/pages/not_found/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class NotFound extends Component {
+    render() {
+        return (
+            <div>
+                Donde vas wachin
+            </div>
+        );
+    }
+}
+
+export default NotFound;
